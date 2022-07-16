da6026969aca08f639381fd1e73de1a75db20922
André de Souza <andreoew@hotmail.com>
2022-07-16 11:11:06 -0300

    update buttons

--- JS/app.js
+++ JS/app.js
@@ -3,6 +3,7 @@ const btnInit = document.querySelector('#btn-init');
 const btnStop = document.querySelector('#btn-stop');
 var myScore = document.querySelector('#my-score');
 var machiScore = document.querySelector('#machine-score');
+var btnReset = document.querySelector('#btn-reset');
 
 let score = 0;
 var scoreMachine = 0;
@@ -13,7 +14,7 @@ var pincel = tela.getContext('2d');
 // pincel.fillStyle = 'lightgray';
 pincel.fillRect(0, 0, 1000, 600);
 
-var raio = 10;
+var raio = 20;
 var xAleatorio;
 var yAleatorio;
 
@@ -25,16 +26,26 @@ buttons.addEventListener('click', function (e) {
         console.log('iniciar');
         tela.style.display = ''; 
         machiScore.innerText = scoreMachine;  
-        myScore.innerText = yourScore;  
-        console.log('meus pontos', yourScore);
-        console.log('maquina', scoreMachine);
+        myScore.innerText = yourScore;
+        // btnInit.style.background = '#00ff11';
+        btnInit.disabled = true;
+        btnStop.disabled = false;
+        btnInit.opacity = 2;      
+
+        // console.log('meus pontos', yourScore);
+        // console.log('maquina', scoreMachine);
 
     } else if (value === btnStop) {
         console.log('Stop');
         tela.style.display = 'none';
+        btnInit.disabled = false;
+        btnStop.disabled = true;
+    }else if (value === btnReset) {
+        console.log('reset clicked');
+        document.location.reload(true);
+    }
 
-    };
-
+    
 });
 
 function desenhaCirculo(x, y, raio, cor) {
@@ -65,10 +76,9 @@ function atualizaTela() {
     desenhaAlvo(xAleatorio, yAleatorio);
 }
 
+
+
 setInterval(atualizaTela, 1000);
-
-
-
 
 function dispara(evento) {
     var x = evento.pageX - tela.offsetLeft;
@@ -78,8 +88,6 @@ function dispara(evento) {
         && (x < xAleatorio + raio)
         && (y > yAleatorio - raio)
         && (y < yAleatorio + raio)) {
-
-
         desenhaCirculo(x, y, raio + 200, 'red');
         yourScore++;
         myScore.innerText = yourScore;
@@ -87,14 +95,17 @@ function dispara(evento) {
         console.log('meus pontos', yourScore);
     } else {
         scoreMachine++;
-        machiScore.innerText = scoreMachine;  
-        console.log('maquina', scoreMachine);
-        
+        machiScore.innerText = scoreMachine;
+        if(scoreMachine >= 3){
+            var divNova = document.createElement('div');
+            var vendedor = document.createTextNode(`O vencedor é o oponente com  ${scoreMachine} pontos`)
+            divNova.appendChild(vendedor);
+
+            var divAtual = document.getElementById('canvas');
+            document.body.insertBefore(divNova, divAtual);
+        }
     }
 
-
-
 }
-
 tela.onclick = dispara;
 
